ca2fc581ce599cd16bcb123981c30571c755f12d
zuozhewangyiping <2261732791@qq.com>
2026-07-18 18:34:14 +0800

    Fix PriorityQueue _type.h name clash with min/max suffixes

--- PriorityQueue/ds_priorityqueue_min_type.h
+++ PriorityQueue/ds_priorityqueue_min_type.h
@@ -14,18 +14,18 @@ typedef struct
 #define DS_PRIORITYQUEUE_MIN_LE(a, b) ((a).data <= (b).data)
 #define DS_PRIORITYQUEUE_MIN_GE(a, b) ((a).data >= (b).data)
 
-static inline void destroy_element(const ds_priorityqueue_min_type *e)
+static inline void destroy_element_min(const ds_priorityqueue_min_type *e)
 {
     (void)e;
 }
-#define DS_PRIORITYQUEUE_MIN_DESTROY_ELEMENT(e) destroy_element(&(e))
+#define DS_PRIORITYQUEUE_MIN_DESTROY_ELEMENT(e) destroy_element_min(&(e))
 
-static inline ds_priorityqueue_min_type clone_element(const ds_priorityqueue_min_type *src, int *judge)
+static inline ds_priorityqueue_min_type clone_element_min(const ds_priorityqueue_min_type *src, int *judge)
 {
     (void)judge;
     return *src;
 }
-#define DS_PRIORITYQUEUE_MIN_CLONE_ELEMENT(e, judge) clone_element(&(e), judge)
+#define DS_PRIORITYQUEUE_MIN_CLONE_ELEMENT(e, judge) clone_element_min(&(e), judge)
 
 #endif
 
@@ -38,16 +38,16 @@ typedef struct
 } ds_priorityqueue_min_type;
 
 // 需要修改 DESTROY_ELEMENT
-static inline void destroy_element(const ds_priorityqueue_min_type *e)
+static inline void destroy_element_min(const ds_priorityqueue_min_type *e)
 {
     free(e->name);
     e->name = NULL;
 }
-#define DS_PRIORITYQUEUE_MIN_DESTROY_ELEMENT(e) destroy_element(&(e))
+#define DS_PRIORITYQUEUE_MIN_DESTROY_ELEMENT(e) destroy_element_min(&(e))
 
 // 需要修改 CLONE_ELEMENT（深拷贝）
 // 编写适配函数，再由宏调用
-static inline ds_priorityqueue_min_type clone_element(const ds_priorityqueue_min_type *src, int *judge)
+static inline ds_priorityqueue_min_type clone_element_min(const ds_priorityqueue_min_type *src, int *judge)
 {
     ds_priorityqueue_min_type copy = {.data = src->data, .name = NULL};
     if (src->name != NULL)
@@ -60,7 +60,7 @@ static inline ds_priorityqueue_min_type clone_element(const ds_priorityqueue_min
     }
     return copy;
 }
-#define DS_PRIORITYQUEUE_MIN_CLONE_ELEMENT(e, judge) clone_element(&(e), judge)
+#define DS_PRIORITYQUEUE_MIN_CLONE_ELEMENT(e, judge) clone_element_min(&(e), judge)
 
 // 比较宏（按 data 比较）。若 data 改为 char *，需同步修改为 strcmp 版本：
 // #define DS_PRIORITYQUEUE_MIN_LT(a, b) (strcmp((a).data, (b).data) < 0)
